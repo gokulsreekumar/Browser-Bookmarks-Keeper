0b4f9faa6792805d1be05a6a74e9b67f3fb8a9d0
gokulsreekumar <gokulsreekumar2@gmail.com>
2021-05-16 20:06:50 +0530

    Adding Functions
    - Disaplaying the prase matched output

--- main.sh
+++ main.sh
@@ -1,3 +1,4 @@
+#!/usr/bin/env bash
 ### Command Line Arguments
 if [ $# -eq 0 ]
     then
@@ -11,29 +12,28 @@ if [ $# -eq 0 ]
         chromium_flag=0
         brave_flag=0
 
+        ## Check whether the Browser App is present
+        ### $1 = FILE location
+        ### $2 = flag
+        check_app_exists() {
+            if [ -e "$1" ]; then
+                local -n flag_ref=$2
+                flag_ref=1
+                echo "$1 exists."
+            else 
+                echo "$1 does not exist."
+            fi
+        }
+
         FILE="/Applications/Google Chrome.app"
-        if [ -e "$FILE" ]; then
-            chrome_flag=1
-            echo "$FILE exists."
-        else 
-            echo "$FILE does not exist."
-        fi
+        check_app_exists "$FILE" chrome_flag 
 
         FILE="/Applications/Chromium.app"
-        if [ -e "$FILE" ]; then
-            chromium_flag=1
-            echo "$FILE exists."
-        else 
-            echo "$FILE does not exist."
-        fi
+        check_app_exists "$FILE" chromium_flag 
 
         FILE="/Applications/Brave Browser.app"
-        if [ -e "$FILE" ]; then
-            brave_flag=1
-            echo "$FILE exists."
-        else 
-            echo "$FILE does not exist."
-        fi
+        check_app_exists "$FILE" brave_flag 
+
 
         echo $chrome_flag $chromium_flag $brave_flag
 
@@ -42,52 +42,36 @@ if [ $# -eq 0 ]
         ## Location where Application Specific Data Files are present: /Users/gokulsreekumar/Library/Application\ Support
         ## Inside this above directory go to the application's folder inside that go to Default and there Bookmarks will be
         
-
         ## Find if BookMarks File Exist
         chrome_bookmark=$(find "/Users/gokulsreekumar/Library/Application Support/Google/Chrome" -name Bookmarks | head -1)
         chromium_bookmark=$(find "/Users/gokulsreekumar/Library/Application Support/Chromium" -name Bookmarks | head -1)
         brave_bookmark=$(find "/Users/gokulsreekumar/Library/Application Support/BraveSoftware" -name Bookmarks | head -1)
 
-        if [ $chrome_flag -eq 1 ] 
-            then
-                if [ -f "$chrome_bookmark" ] 
-                    then
-                        cat "$chrome_bookmark" | grep url | grep -v type | awk '{print($2)}' > chrome_bookmarks.txt
-                fi
-        fi
+        ## Extract bookmark urls
+        ### $1 => flag
+        ### $2 => bookmark
+        ### $3 => filename to store bookmarks
+        extract_bookmark() {
+            if [ $1 -eq 1 ] 
+                then
+                    if [ -f "$2" ]
+                        then
+                            cat "$2" | grep url | grep -v type | awk '{print($2)}' > ${3}.txt
+                    fi
+            fi
+        }
 
-        if [ $chromium_flag -eq 1 ] 
-            then 
-                if [ -f "$chromium_bookmark" ] 
-                    then
-                        cat "$chromium_bookmark" | grep url | grep -v type | awk '{print($2)}' > chromium_bookmarks.txt
-                fi
-        fi
+        extract_bookmark $chrome_flag "$chrome_bookmark" "chrome_bookmarks"
+        extract_bookmark $chromium_flag "$chromium_bookmark" "chromium_bookmarks"
+        extract_bookmark $brave_flag "$brave_bookmark" "brave_bookmarks"
 
-        if [ $brave_flag -eq 1 ] 
-            then 
-                if [ -f "$brave_bookmark" ] 
-                    then
-                        cat "$brave_bookmark" | grep url | grep -v type | awk '{print($2)}' > brave_bookmarks.txt
-                fi
-        fi
-
-
-
-        # chrome_bookmark='/Users/gokulsreekumar/Library/Application Support/Google/Chrome/Default/Bookmarks'
-        # chromium_bookmark='/Users/gokulsreekumar/Library/Application Support/Chromium/Default/Bookmarks'
-        # brave_bookmark="/Users/gokulsreekumar/Library/Application\ Support/BraveSoftware/Brave-Browser/Default/Bookmarks"
-
-
-        # if [ $chrome_flag -eq 1 ] 
-        #     then
-        #         cat "$chrome_bookmark" | grep url | grep -v type | awk '{print($2)}' > chrome_bookmarks.txt
-        # fi
         
-        # if [ $chromium_flag -eq 1 ] 
-        #     then
-        #         cat "$chromium_bookmark" | grep url | grep -v type | awk '{print($2)}' > chromium_bookmarks.txt
-        # fi
-
+        ## Search for the Phrase
+        echo "Results from Google Chrome:"
+        grep $1 chrome_bookmarks.txt
+        echo "Results from Chromium:"
+        grep $1 chromium_bookmarks.txt
+        echo "Results from Brave:"
+        grep $1 brave_bookmarks.txt
     
 fi
